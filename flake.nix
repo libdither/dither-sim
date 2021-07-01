ef7d2543cd4d0d5fd21909c50035d8f4005d0bd4
zyansheep <zyansheep@github.com>
2021-07-01 16:19:50 -0400

    use better rust overlay for nix

--- flake.nix
+++ flake.nix
@@ -1,30 +1,25 @@
 {
   	inputs = {
+		nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+		rust-overlay.url = "github:oxalica/rust-overlay";
 		utils.url = "github:numtide/flake-utils";
-		naersk.url = "github:nmattia/naersk";
-		fenix.url = "github:nix-community/fenix";
   	};
 
-  	outputs = { self, nixpkgs, utils, naersk, fenix }:
+  	outputs = { self, nixpkgs, utils, rust-overlay, ... }:
 	utils.lib.eachDefaultSystem (system: let
-		pkgs = nixpkgs.legacyPackages."${system}";
-		# Specify Rust Toolchain
-		# Use Stable (Default)
-		# naersk-lib = naersk.lib."${system}";
-		# Use Nightly (provided by fenix)
-		naersk-lib = naersk.lib."${system}".override {
-			# Use Fenix to get nightly rust
-			inherit (fenix.packages.${system}.minimal) cargo rustc;
-		};
+		overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
 	in rec {
-		# `nix build`
-		packages.dbr-sim = naersk-lib.buildPackage {
-			pname = "dbr-sim";
-			root = ./.;
+		# `nix develop`
+		devShell = pkgs.mkShell {
 			buildInputs = with pkgs; [
+				pkgs.rust-bin.nightly.latest.default
 				cmake
 				pkgconfig
 				stdenv.cc.cc.lib
+				lld
 				
 				x11
 				xorg.libXcursor
@@ -38,21 +33,8 @@
 				fontconfig
 				freetype
 			];
-		};
-		defaultPackage = packages.dbr-sim;
-
-		# `nix run`
-		apps.dbr-sim = utils.lib.mkApp {
-			drv = packages.dbr-sim;
-		};
-		defaultApp = apps.dbr-sim;
-
-		# `nix develop`
-		devShell = pkgs.mkShell {
-			buildInputs = packages.dbr-sim.buildInputs ++ [ pkgs.lld ];
-			LD_LIBRARY_PATH = "${nixpkgs.lib.makeLibraryPath packages.dbr-sim.buildInputs}";
-			hardeningDisable = [ "fortify" ];
-			NIX_CFLAGS_LINK = "-fuse-ld=lld";
+			#hardeningDisable = [ "fortify" ];
+			#NIX_CFLAGS_LINK = "-fuse-ld=lld";
 		};
 	});
 }
